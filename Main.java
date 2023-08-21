a299a5c86c11c8cceb8b4670b3c4705ca3eb8f9c
Stephanie Caldera <stephanie.caldera123@gmail.com>
2023-08-20 21:17:05 -0300

    modificacion a la funcion dia en ano bisiesto

--- Main.java
+++ Main.java
@@ -8,24 +8,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("ingrese el dia: ");   
+		System.out.println("ingrese el dia: ");
 		int dia = sc.nextInt();
 		System.out.println("ingrese el mes: ");
 		int mes = sc.nextInt();
 		System.out.println("ingrese el año: ");
 		int año = sc.nextInt();
-		if (año >= 1900 && año <= 2099) {  //rango de año solicitado
-			if ((año % 4 == 0) && (año % 100 !=0 || año % 400 == 0)) { //tres condiciones a cumplir para verificar si el año es bisiesto
-				System.out.println(año+ " es un año bisiesto");
-			}else {
-				System.out.println(año+ " no es un año bisiesto");
-			}
+		if (año >= 1900 && año <= 2099) { // rango de año solicitado
 			if (mes >= 1 && mes <= 12) {
 				if (mes == 2) {
-					if (dia >= 1 && dia <= 28) {
-						System.out.println("fecha valida");
+					if ((dia >= 1 && dia <= 28)
+							|| (dia == 29 && (año % 4 == 0) && (año % 100 != 0 || año % 400 == 0))) { // si es dia 29 y cumple con la condicion de año bisiesto
+						System.out.println("año bisiesto, fecha valida");
 					} else {
-						System.out.println("dia invalido");
+						System.out.println("año no bisiesto, fecha invalida");
 					}
 				} else {
 					if (dia >= 1 && dia <= 30) {
